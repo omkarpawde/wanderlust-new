586950629da8d274a351907190a3b784d4aa28e5
Omkar Pawde <omkarpawde964@gmail.com>
2026-06-19 09:44:21 +0530

    Make project responsive and update navbar

--- init/index.js
+++ init/index.js
@@ -1,3 +1,5 @@
+require("dotenv").config({ path: "../.env" });
+
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listings.js");
@@ -5,22 +7,21 @@ const Listing = require("../models/listings.js");
 main()
   .then(() => {
     console.log("connection successfully");
+    initDB();
   })
   .catch((err) => {
     console.log(err);
   });
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/wondurlust");
+  await mongoose.connect(process.env.ATLASDB_URL);
 }
 
 const initDB = async () => {
   await Listing.deleteMany({});
   initData.data = initData.data.map((obj) => ({
     ...obj,
-    owner: "6a28e96be92a3463643566fb",
+    owner: "omkarpawde964_db_user",
   }));
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
-
-initDB();
